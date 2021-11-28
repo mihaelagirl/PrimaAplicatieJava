cc597712366ab6c81c71b38df4b36e3584439d77
MIHAELA <mihaelasapunaru10@yahoo.com>
2021-11-28 19:38:31 +0200

    Modificare Main.Java - stergere comentariu

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -2,7 +2,7 @@ package ro.java.ctrln;
 
 public class Main {
 
-    // Java_Home
+    
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
